27ed995bdc29cfb5d1a24161266ec9d7a042e34e
Ebrar <16155052@mersin.edu.tr>
2023-04-06 15:18:02 +0300

    axios ,db added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,42 @@
 <template>
   <div class="home">
-   <h2>hello first project</h2>
+   <div v-if="projects.length">
+    <div v-for="project in projects" :key="project.id">
+       <p>{{ project.title }}</p>
+    
+    
+    </div>
+
+   </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import axios from 'axios';
 
 export default {
   name: 'HomeView',
   components: {
 
+  },
+  data(){
+    return {
+      projects:[]
+  }
+},
+  mounted(){
+  
+    axios.get('http://localhost:3000/projects')
+  .then(response => {
+    this.projects = response.data;
+  })
+  .catch(error => {
+    console.log(error.message);
+  });
   }
 }
 </script>
+
+
