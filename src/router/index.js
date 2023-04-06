563cf5c094ceb991636c7774c5640a60ce1bb688
Ebrar <16155052@mersin.edu.tr>
2023-04-06 14:39:32 +0300

    add views

--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,22 @@ import HomeView from '../views/HomeView.vue'
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/add',
+    name: 'AddProject',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AddProject.vue')
+  },
+  {
+    path: '/projects/:id',
+    name: 'EditProject',
+    component: () => import(/* webpackChunkName: "about" */ '../views/EditProject.vue'),
+    props: true
   }
 ]
 
